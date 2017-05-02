822102fea1cdb1373e83f8398caf7c13cd597a41
siekiera92 <piotr.siekierski.92@gmail.com>
2017-05-02 14:27:54 +0200

    1

---
+++ www/js/pgb.js
@@ -0,0 +1,46 @@
+function init() {
+	document.addEventListener("deviceready",onDeviceReady, false);
+}
+
+function onDeviceReady() {
+	navigator.notification.beep(1);
+}
+
+var app = {
+    sendSms: function() {
+        var number = document.getElementById('numberTxt').value;
+        var message = document.getElementById('messageTxt').value;
+        console.log("number=" + number + ", message= " + message);
+
+        //CONFIGURATION
+        var options = {
+            replaceLineBreaks: false, // true to replace \n by a new line, false by default
+            android: {
+                intent: 'INTENT'  // send SMS with the native android SMS messaging
+                //intent: '' // send SMS without open any other app
+            }
+        };
+
+        var success = function () { alert('Message sent successfully'); };
+        var error = function (e) { alert('Message Failed:' + e); };
+        sms.send(number, message, options, success, error);
+    }
+};
+
+var app = {
+    checkSMSPermission: function() {
+        var success = function (hasPermission) { 
+            if (hasPermission) {
+                sms.send(...);
+            }
+            else {
+
+            	alert('Potrzebne pozwolenie')
+                // show a helpful message to explain why you need to require the permission to send a SMS
+                // read http://developer.android.com/training/permissions/requesting.html#explain for more best practices
+            }
+        };
+        var error = function (e) { alert('Something went wrong:' + e); };
+        sms.hasPermission(success, error);
+    }
+};
